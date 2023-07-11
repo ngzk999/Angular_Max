fec873255bc509275618c6872e1fee466154dfed
SDPzk <n.zheng.keong@simedarbyplantation.com>
2023-07-11 22:13:14 +0800

    create new project + bootstrap

---
+++ assign_4_property_event_binding_view_encapsulation/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assign_4_property_event_binding_view_encapsulation';
+}
