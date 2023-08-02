5c239678f61ff8c824acb23a7f1486b992c3d01f
SDPzk <n.zheng.keong@simedarbyplantation.com>
2023-08-02 23:40:35 +0800

    shopping list service

--- course_project_the_basics/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course_project_the_basics/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,9 +10,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInput: ElementRef;
   @ViewChild('amountInput') amountInput: ElementRef;
-  @Output() addIngredientEvent = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +22,7 @@ export class ShoppingEditComponent implements OnInit {
       amount: this.amountInput.nativeElement.value,
     };
 
-    this.addIngredientEvent.emit(ingredientToAdd);
+    this.shoppingListService.addIngredients(ingredientToAdd);
   }
 
 }
